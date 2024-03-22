08fe291b912a6a49d67acc5986edf0b769a37e22
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-22 19:54:30 -0300

    fix(frontend/src/view/CategoryEditView): simplifica lógica + corrige erro de não editar categorias

--- frontend/src/views/CategoryEditView.vue
+++ frontend/src/views/CategoryEditView.vue
@@ -1,51 +1,41 @@
 <template>
-    <div>
-      <h1>Editar Categoria</h1>
-      <CategoryForm :initialCategory="category" @save="saveCategory"/>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, watch } from 'vue';
-  import { useRouter } from 'vue-router';
-  import { CategoryService } from '../api/CategoryService';
-  import type { Category } from '../types/category';
-  
-  const props = defineProps({
-    initialCategory: {
-      type: Object as () => Category | null,
-      default: null,
-    },
-  });
-  
-  const emit = defineEmits(['save']);
-  
-  const category = ref<Category>({
-    id: props.initialCategory?.id || 0,
-    title: props.initialCategory?.title || '',
-    description: props.initialCategory?.description || '',
-  });
-  
-  watch(
-    () => props.initialCategory,
-    (newVal) => {
-      if (newVal) {
-        category.value = { ...newVal };
-      }
-    },
-    { deep: true }
-  );
-  
-  const router = useRouter();
-  
-  const saveCategory = async () => {
-    if (category.value.id) {
-      await CategoryService.updateCategory(category.value.id, category.value);
-    } else {
-      await CategoryService.createCategory(category.value);
+  <div>
+    <h1>{{ isEditMode ? 'Editar' : 'Criar' }} Categoria</h1>
+    <CategoryForm :initialCategory="categoryData" @update="handleUpdate" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import CategoryForm from '../components/CategoryForm.vue';
+import { CategoryService } from '../api/CategoryService';
+import type { Category } from '../types/category';
+
+const router = useRouter();
+const route = useRoute();
+const categoryData = ref<Category | null>(null);
+
+const fetchCategory = async () => {
+  const id = parseInt(route.params.id as string);
+  if (id) {
+    const data = await CategoryService.getCategoryById(id);
+    if (data) {
+      categoryData.value = data;
     }
-    emit('save');
-    router.push('/categories');
-  };
-  </script>
-  
+  }
+};
+
+onMounted(fetchCategory);
+
+const isEditMode = computed(() => route.params.id != null);
+
+const handleUpdate = async (updatedCategory: Category) => {
+  if (updatedCategory.id) {
+    await CategoryService.updateCategory(updatedCategory.id, updatedCategory);
+  } else {
+    await CategoryService.createCategory(updatedCategory);
+  }
+  router.push('/categories');
+};
+</script>
