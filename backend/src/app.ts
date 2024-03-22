84d9a83375fa9ce136c90991b28acb024baa1311
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-22 17:01:59 -0300

    chore: install cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,14 @@ import categoryRoutes from './routes/categoryRoutes';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', categoryRoutes);
 app.use(errorHandler);
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Servidor rodando!');
