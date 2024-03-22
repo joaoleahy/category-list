78823737942a1b4b5a50009aef8f7f1a1300dad0
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-22 13:20:29 -0300

    chore(backend): instala as dependências necessárias + configura jest para typescript

---
+++ backend/jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // suas outras configurações
+};
+
+export default config;
