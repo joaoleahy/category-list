4ea7db4acffa50ec12a940fc05637eb89f9a64df
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-22 13:19:32 -0300

    feat (backend/src/app): atualiza inicialização do app com novos middlewares

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import categoryRoutes from './routes/categoryRoutes';
+import { errorHandler } from './middleware/errorHandler';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api', categoryRoutes);
+app.use(errorHandler);
+
+
+app.get('/', (req, res) => {
+    res.send('Servidor rodando!');
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Servidor escutando na porta ${PORT}`);
+  });
+  
+
+export default app;
